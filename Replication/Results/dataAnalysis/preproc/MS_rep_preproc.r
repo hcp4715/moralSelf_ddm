d2780ad01fb3fb255a4e7faae71e328c5679fd10
hcp4715 <hcp4715@gmail.com>
2019-01-29 09:19:52 +0100

    plot acc and rt dist. in one plot

--- Replication/Results/dataAnalysis/preproc/MS_rep_preproc.r
+++ Replication/Results/dataAnalysis/preproc/MS_rep_preproc.r
@@ -130,25 +130,25 @@ excldSub_M   <- c(excldSub1_M, excldSub2_M,excldSub3_M) # 7302, 7303
 excldSub_C   <- c(excldSub1_C, excldSub2_C,excldSub3_C) # 7302, 7303, 7338
 
 # select valid data for further analysis
-df.M1.valid <- df.M %>%
+df.M1.V <- df.M %>%
    dplyr::mutate(ACC = ifelse(ACC == 1, 1, 0))  %>%  # no response as wrong
    dplyr::filter(Subject,!Subject %in% excldSub_M)   # exclude the invalid subjects
 
-df.C1.valid <- df.C %>%
+df.C1.V <- df.C %>%
    dplyr::mutate(ACC = ifelse(ACC == 1, 1, 0))  %>%  # no response as wrong
    dplyr::filter(Subject,!Subject %in% excldSub_C)   # exclude the invalid subjects
 
 # check the number of participants are correct
-length(unique(df.M1.valid$Subject)) + length(excldSub_M) == length(unique(df.M$Subject))
-length(unique(df.C1.valid$Subject)) + length(excldSub_C) == length(unique(df.C$Subject))
+length(unique(df.M1.V$Subject)) + length(excldSub_M) == length(unique(df.M$Subject))
+length(unique(df.C1.V$Subject)) + length(excldSub_C) == length(unique(df.C$Subject))
 
 
 # excluded correct trials with < 200ms RT
-ratio.excld.trials.M <- nrow(df.M1.valid[df.M1.valid$RT*1000 <= 200 & df.M1.valid$ACC == 1,])/nrow(df.M1.valid)  # ratio of invalid trials
-ratio.excld.trials.C <- nrow(df.C1.valid[df.C1.valid$RT*1000 <= 200 & df.C1.valid$ACC == 1,])/nrow(df.C1.valid)
+ratio.excld.trials.M <- nrow(df.M1.V[df.M1.V$RT*1000 <= 200 & df.M1.V$ACC == 1,])/nrow(df.M1.V)  # ratio of invalid trials
+ratio.excld.trials.C <- nrow(df.C1.V[df.C1.V$RT*1000 <= 200 & df.C1.V$ACC == 1,])/nrow(df.C1.V)
 
-df.M1.valid <- df.M1.valid %>% dplyr::filter(!(RT <= 0.2 & ACC==1)) # filter invalid trials
-df.C1.valid <- df.C1.valid %>% dplyr::filter(!(RT <= 0.2 & ACC==1)) # filter invalid trials
+df.M1.V <- df.M1.V %>% dplyr::filter(!(RT <= 0.2 & ACC==1)) # filter invalid trials
+df.C1.V <- df.C1.V %>% dplyr::filter(!(RT <= 0.2 & ACC==1)) # filter invalid trials
 
 ## Basic information of the data ####
 df.M1.T.basic <- df.M %>%
@@ -161,7 +161,7 @@ df.M1.T.basic <- df.M %>%
                     Age_sd   = round(sd(Age),2))
 
 # valide data for matching task
-df.M1.V.basic <- df.M1.valid %>%
+df.M1.V.basic <- df.M1.V %>%
    dplyr::select(Subject, Age, Sex) %>%
    dplyr::distinct(Subject, Age, Sex) %>%
    dplyr::summarise(subj_N = length(Subject),
@@ -171,7 +171,7 @@ df.M1.V.basic <- df.M1.valid %>%
                     Age_sd   = round(sd(Age),2))
 
 # valid data for categorization task
-df.C1.V.basic <- df.C1.valid %>%
+df.C1.V.basic <- df.C1.V %>%
    dplyr::select(Subject, Age, Sex) %>%
    dplyr::distinct(Subject, Age, Sex) %>%
    dplyr::summarise(subj_N = length(Subject),
@@ -185,13 +185,16 @@ df.C1.V.basic <- df.C1.valid %>%
 ###########################################################
 # exclusion trials, criterion: trials without response or wrong key
 # Note: we didn't remove the correct response less than 200 ms
+##################################################
+### preparing the data for Stimuli-code hddm  ####
+##################################################
 
 df.M.hddm_m <- df.M %>%                                 # start from the valid data, RT in seconds.
    dplyr::filter(Subject,!Subject %in% excldSub_M) %>%
    dplyr::filter(ACC == 1 | ACC == 0) %>%               # exclude trials without response or with wrong keys
    dplyr::filter(Match == 'match') %>%
-   dplyr::select(Subject,Morality,Identity,RT,ACC) %>%  # select column
-   dplyr::rename(subj_idx = Subject, val = Morality, id = Identity, rt = RT, response = ACC) # rename column
+   dplyr::select(Subject,Match, Morality,Identity,RT,ACC) %>%  # select column
+   dplyr::rename(subj_idx = Subject, match = Match, val = Morality, id = Identity, rt = RT, response = ACC) # rename column
 
 df.M.hddm_nm <- df.M %>%
    dplyr::filter(Subject,!Subject %in% excldSub_M) %>%
@@ -199,6 +202,15 @@ df.M.hddm_nm <- df.M %>%
    dplyr::filter(Match == 'mismatch') %>%
    dplyr::select(Subject,Morality,Identity,RT,ACC) %>%  # select column
    dplyr::rename(subj_idx = Subject, val = Morality, id = Identity, rt = RT, response = ACC) # rename column
+
+df.M.hddm_stim <- df.M %>%
+   dplyr::filter(Subject,!Subject %in% excldSub_M) %>%
+   dplyr::filter(ACC == 1 | ACC == 0) %>%               # exclude trials without response or with wrong keys
+   dplyr::mutate(response = ifelse((Match == "match" & ACC ==1) | (Match == "mismatch" & ACC ==0), 1, 0)) %>%
+   dplyr::mutate(stim = ifelse(Match == "match", 1, 0)) %>%
+   dplyr::select(Subject,Match,Morality,Identity,stim,response,RT) %>%  # select column
+   dplyr::rename(subj_idx = Subject, match = Match, val = Morality, id = Identity, rt = RT) # rename column
+
 
 ##################################################
 ### preparing the data for Stimuli-code hddm  ####
@@ -226,10 +238,11 @@ df.C.hddm_id_stim <- df.C%>%
 setwd(ddmDir)
 write.csv(df.M.hddm_m,'MS_match_hddm.csv',row.names = F)
 write.csv(df.M.hddm_nm,'MS_mismatch_hddm.csv',row.names = F)
+write.csv(df.M.hddm_stim,'MS_match_hddm_stim.csv',row.names = F)
 write.csv(df.C.hddm_val_stim,'MS_categ_val_hddm_stim.csv',row.names = F)
 write.csv(df.C.hddm_id_stim,'MS_categ_id_hddm_stim.csv',row.names = F)
 setwd(curDir)
-rm('df.M.hddm_m','df.M.hddm_nm','df.C.hddm_val_stim','df.C.hddm_id_stim')
+rm('df.M.hddm_m','df.M.hddm_nm','df.M.hddm_stim','df.C.hddm_val_stim','df.C.hddm_id_stim')
 
 
 # ---------------------------------------------------------------------------------------
@@ -246,7 +259,7 @@ df.M1.V.acc  <- df.M1.V %>%
                        ACC = sum(ACC)/length(ACC))
 
 # transform to wide-format
-df.M1.V.acc_w <- dcast(df.M1.V.acc, Subject ~ Match + Morality + Identity,value.var = "ACC")
+df.M1.V.acc_w <- reshape2::dcast(df.M1.V.acc, Subject ~ Match + Morality + Identity,value.var = "ACC")
 
 # rename the column number
 colnames(df.M1.V.acc_w)[2:9] <- paste("ACC", colnames(df.M1.V.acc_w[,2:9]), sep = "_")
@@ -259,25 +272,29 @@ df.M1.V$sdt <- NA
 for (i in 1:nrow(df.M1.V)){
       if (df.M1.V$ACC[i] == 1 & df.M1.V$Match[i] == "match"){
             df.M1.V$sdt[i] <- "hit"
-      } else if (df.M1.V$ACC[i] == 1 & df.M1.V$Match[i] == "nonmatch" ){
+      } else if (df.M1.V$ACC[i] == 1 & df.M1.V$Match[i] == "mismatch" ){
             df.M1.V$sdt[i] <- "CR"
       } else if (df.M1.V$ACC[i] == 0 & df.M1.V$Match[i] == "match"){
             df.M1.V$sdt[i] <- "miss"
-      } else if (df.M1.V$ACC[i] == 0 & df.M1.V$Match[i] == "nonmatch" ){
+      } else if (df.M1.V$ACC[i] == 0 & df.M1.V$Match[i] == "mismatch" ){
             df.M1.V$sdt[i] <- "FA"
       }
 }
 
 # calculate the number of each for each condition
-df.M1.V.SDT <- plyr::ddply(df.M1.V,.(Subject,Age, Gender, Morality, Identity,sdt), summarise, N = length(sdt))
-df.M1.V.SDT <- df.M1.V.SDT[complete.cases(df.M1.V.SDT$sdt),] # exclude NA, which represents no-response trials
+df.M1.V.SDT <- df.M1.V %>%
+   dplyr::group_by(Subject,Age, Sex, Morality, Identity,sdt) %>%
+   dplyr::summarise(N = length(sdt)) %>%
+   dplyr::filter(!is.na(sdt))           # no NAs
 
 # long format to wide
-df.M1.V.SDT_w <- reshape2::dcast(df.M1.V.SDT, Subject + Age + Gender + Morality + Identity ~ sdt,value.var = "N")
-df.M1.V.SDT_w$miss[is.na(df.M1.V.SDT_w$miss)] <- 0 # transfer NA to 0
-df.M1.V.SDT_w$FA[is.na(df.M1.V.SDT_w$FA)]     <- 0
-df.M1.V.SDT_w$hitR <- df.M1.V.SDT_w$hit/(df.M1.V.SDT_w$hit + df.M1.V.SDT_w$miss)
-df.M1.V.SDT_w$faR <- df.M1.V.SDT_w$FA/(df.M1.V.SDT_w$FA + df.M1.V.SDT_w$CR)
+df.M1.V.SDT_w <- reshape2::dcast(df.M1.V.SDT, Subject + Age + Sex + Morality + Identity ~ sdt,value.var = "N")
+df.M1.V.SDT_w <- df.M1.V.SDT_w %>%
+   dplyr::mutate(miss = ifelse(is.na(miss),0,miss), # if not miss trial, to 0
+                 FA   = ifelse(is.na(FA),0,FA),     # if not FA trial, to 0
+                 hitR = hit/(hit + miss),           # calculate the hit rate
+                 faR  = FA/(FA+CR))                 # calculate the FA rate
+
 
 # standardized way to deal with the extreme values
 for (i in 1:nrow(df.M1.V.SDT_w)){
@@ -296,7 +313,7 @@ for (i in 1:nrow(df.M1.V.SDT_w)){
 df.M1.V.SDT_w$dprime <- mapply(dprime,df.M1.V.SDT_w$hitR,df.M1.V.SDT_w$faR)
 
 # transfor from long format to wide format
-df.M1.V.SDT_ww <- dcast(df.M1.V.SDT_w, Subject + Age + Gender ~ Morality + Identity ,value.var = "dprime")
+df.M1.V.SDT_ww <- reshape2::dcast(df.M1.V.SDT_w, Subject + Age + Sex ~ Morality + Identity ,value.var = "dprime")
 # rename the column number
 colnames(df.M1.V.SDT_ww)[4:7] <- paste("d", colnames(df.M1.V.SDT_ww[,4:7]), sep = "_")
 
@@ -310,7 +327,7 @@ df.M1.V.RT <- df.M1.V[df.M1.V$ACC == 1,]
 
 # get the summary results
 df.M1.V.RT.subj <- summarySEwithin(df.M1.V.RT,measurevar = 'RT', withinvar = c('Subject','Match','Morality','Identity'),idvar = 'Subject', na.rm = TRUE)
-df.M1.V.RT.subj_w <- dcast(df.M1.V.RT.subj, Subject ~ Match + Morality + Identity ,value.var = "RT") 
+df.M1.V.RT.subj_w <- reshape2::dcast(df.M1.V.RT.subj, Subject ~ Match + Morality + Identity ,value.var = "RT") 
 
 # rename the columns of RT data
 colnames(df.M1.V.RT.subj_w)[2:9] <- paste("RT", colnames(df.M1.V.RT.subj_w[,2:9]), sep = "_")
@@ -329,6 +346,124 @@ colnames(df.M1.V.sum_rt_acc_l) <- c("Subject","Match","Morality",'Identity',"Ntr
 
 # order the columns
 df.M1.V.sum_w <- df.M1.V.sum_w[,c(colnames(df.M1.V.sum_w)[c(1,10:11,2:9,12:23)])]
+
+### plot the density of d prime and rt
+# Create a scatterplot with density margin plots
+
+
+
+## prepare the data
+df.M1.V.SDT_l$Cond <- paste(df.M1.V.SDT_l$Morality,df.M1.V.SDT_l$Identity,sep = '_')
+df.M1.V.RT_m <- df.M1.V.sum_rt_acc_l %>%
+   dplyr::filter(Match == 'match') %>%
+   dplyr::mutate(Cond = paste(Morality, Identity, sep = '_')) %>%
+   dplyr::mutate(RT = RT*1000)
+
+df.M1.V_dens_p <- merge(df.M1.V.SDT_l, df.M1.V.RT_m)
+
+# The plotHolder() function from C-3PR creates a blank plot template that will hold the figures
+plotHolder <- function(useArial = F,afmPATH="~/Dropbox"){
+   require(ggplot2)
+   ggplot() +
+      geom_blank(aes(1,1)) +
+      theme(line = element_blank(),
+            text  = element_blank(),
+            title = element_blank(),
+            plot.background = element_blank(),
+            #           panel.grid.major = element_blank(),
+            #           panel.grid.minor = element_blank(),
+            panel.border = element_blank(),
+            panel.background = element_blank()
+            #           axis.title.x = element_blank(),
+            #           axis.title.y = element_blank(),
+            #           axis.text.x = element_blank(),
+            #           axis.text.y = element_blank(),
+            #           axis.ticks = element_blank()
+      )
+}
+
+gg.theme <- function(type=c("clean","noax")[1],useArial = F, afmPATH="~/Dropbox"){
+   require(ggplot2)
+   if(useArial){
+      set.Arial(afmPATH)
+      bf_font="Arial"
+   } else {bf_font="Helvetica"}
+   
+   switch(type,
+          clean = theme_bw(base_size = 16, base_family=bf_font) +
+             theme(axis.text.x     = element_text(size = 14),
+                   axis.title.y    = element_text(vjust = +1.5),
+                   panel.grid.major  = element_blank(),
+                   panel.grid.minor  = element_blank(),
+                   legend.background = element_blank(),
+                   legend.key = element_blank(),
+                   panel.border = element_blank(),
+                   panel.background = element_blank(),
+                   axis.line  = element_line(colour = "black")),
+          
+          noax = theme(line = element_blank(),
+                       text  = element_blank(),
+                       title = element_blank(),
+                       plot.background = element_blank(),
+                       panel.border = element_blank(),
+                       panel.background = element_blank())
+   )
+}
+
+
+blankPlot <- plotHolder()
+
+# X margin density plot (note: gg.theme() from C-3PR can be used directly in a ggplot2() call)
+
+xDense <- ggplot(df.M1.V_dens_p, aes(x=dprime, fill=Cond)) + 
+   geom_density(aes(y= ..count..),trim=F,alpha=.5) + 
+   xlab("") + ylab("") + xlim(0,5) +
+   gg.theme("noax") + 
+   theme(legend.position = "none",plot.margin = unit(c(0,0,0,4), "lines"))
+
+## Uncomment to save subplot
+# ggsave("RPP_F3_xDense.png",plot=xDense)
+
+# Y margin density plot (note: gg.theme() from C-3PR can be used directly in a ggplot2() call)
+
+yDense <- ggplot(df.M1.V_dens_p, aes(x=RT, fill=Cond)) + 
+   geom_density(aes(y= ..count..),trim=F,alpha=.5) + 
+   xlab("") + ylab("") + xlim(400,900) + 
+   coord_flip() + 
+   gg.theme("noax") + 
+   theme(legend.position = "none", plot.margin = unit(c(0,0,3,0), "lines")) 
+
+## Uncomment to save subplot
+# ggsave("RPP_F3_yDense.png",plot=yDense)
+
+# The main scatterplot (note: gg.theme() from C-3PR can be used directly in a ggplot2() call)
+scatterP<-
+   ggplot(df.M1.V_dens_p,aes(x=dprime,y=RT)) +  
+   #geom_hline(aes(yintercept=0),linetype=2) +
+   #geom_abline(intercept=0,slope=1,color="Grey60")+
+   geom_point(aes(size= 0.8,fill=Cond),color="Grey30",shape=21,alpha=.8) + 
+   #geom_rug(aes(color=Cond),size=1,sides="b",alpha=.6) + 
+   #geom_rug(aes(color=Cond),,size=1,sides="l",alpha=.6) + 
+   scale_x_continuous(name="d prime",limits=c(0,5),breaks=c(0,1,2,3,4,5)) + 
+   scale_y_continuous(name="RT",limits=c(400,900),breaks=c(400,500,600,700,800,900)) + 
+   ggtitle("") + xlab("") + ylab("") + 
+   #scale_size_continuous(name="Replication Power",range=c(2,9)) + 
+   scale_color_discrete(name='Conditions') +
+   #scale_fill_discrete(name="p-value") +
+   gg.theme("clean") + 
+   theme(legend.position=c(.9,.6), plot.margin = unit(c(-2,-1.5,2,2), "lines")) 
+
+## Uncomment to save subplot
+# ggsave("RPP_F3_scatter.png",plot=scatterP)
+
+# Yet another way to organise plots: grid.arrange() from the gridExtra package.
+gridExtra::grid.arrange(xDense, blankPlot, scatterP, yDense, ncol=2, nrow=2, widths=c(4, 1.4), heights=c(1.4, 4))
+
+# SAVE combined plots as PDF
+pdf("Figure1_RT_drpime.pdf",pagecentre=T, width=15,height=12 ,paper = "special")
+gridExtra::grid.arrange(xDense, blankPlot, scatterP, yDense, ncol=2, nrow=2, widths=c(4, 1.4), heights=c(1.4, 4))
+dev.off()
+
 
 ####################################
 #####  effects in matching  ########
@@ -361,7 +496,7 @@ setwd(curDir)
 ###############   ACC    ###########
 ####################################
 
-df.C1.V.acc <- plyr::ddply(df.C1.V,.(Subject,Age, Gender, Task,Morality,Identity), summarise,
+df.C1.V.acc <- plyr::ddply(df.C1.V,.(Subject,Age, Sex, Task,Morality,Identity), summarise,
                            N = length(ACC),
                            countN = sum(ACC),
                            ACC = sum(ACC)/length(ACC))
