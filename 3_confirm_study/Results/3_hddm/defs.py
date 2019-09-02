53c41550e133a7eb494c41cc70f5dbe12c3cc6ef
hcp4715 <hcp4715@gmail.com>
2019-09-02 08:49:24 +0200

    revised the code based on comments

---
+++ 3_confirm_study/Results/3_hddm/defs.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def createandsort (n):
+ rand = np.random.RandomState(42) #Give a seed to reproduce results
+ a = rand.rand(n) #Generate an array of size n
+ return a.sort() #Sort the array
---
+++ 3_confirm_study/Results/3_hddm/workers.py
@@ -0,0 +1,2 @@
+def worker(x):
+    return x*x
